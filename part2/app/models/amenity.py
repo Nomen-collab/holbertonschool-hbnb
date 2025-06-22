243f4833a23e2cb377459412d08688ae40a5f131
Trice97 <bolinp@gmail.com>
2025-06-22 17:51:52 +0200

    hbhb_classes-corrections

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -3,12 +3,23 @@ from app.models.base_model import BaseModel
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
+        if len(name) > 50:
+            raise ValueError("name must be 50 characters or less")
         self.name = name
+
 
     def to_dict(self):
         return {
-            'id': self.id,
+            'id': self.id,  # ✅ Ajout des deux-points
             'name': self.name,
             'created_at': self.created_at.isoformat(),
             'updated_at': self.updated_at.isoformat()
         }
+
+
+    def update(self, data):
+        """Function to update amenities attributes"""
+        for key, value in data.items():
+            if hasattr(self, key) and key != 'id':  # ✅ Parenthèse corrigée
+                setattr(self, key, value)  # ✅ setattr + value en minuscule
+        self.save()
--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -3,6 +3,14 @@ from app.models.base_model import BaseModel
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, password="", is_admin=False):
         super().__init__()
+
+        if len(first_name) > 50:
+            raise ValueError("first name too long")
+        if len(last_name) > 50:
+            raise ValueError("Last name too long")
+        if "@" not in email:
+            raise ValueError("invalid Email")
+
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
@@ -19,3 +27,10 @@ class User(BaseModel):
             'created_at': self.created_at.isoformat(),
             'updated_at': self.updated_at.isoformat()
         }
+
+    def update(self, data):
+    """Update user attributes"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
